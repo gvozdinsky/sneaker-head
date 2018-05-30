7a6d21b4a080ded8d9990f9d59a3984c0514c020
dima2395 <gdima2395@gmail.com>
2018-05-30 19:06:31 +0300

    trying to connect to mlab db

--- server/app.js
+++ server/app.js
@@ -20,71 +20,75 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 //set mongodb connection
-const connectionString = process.env.DATABASE_URL || 'mongodb://localhost/shopper';
-// const dbOptions = {
-//   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-//   reconnectInterval: 500, // Reconnect every 500ms
-//   poolSize: 10, // Maintain up to 10 socket connections
-//   // If not connected, return errors immediately rather than waiting for reconnect
-//   bufferMaxEntries: 0
-// };
+const connectionString = 'mongodb://dima2395:dimastii1@ds141720.mlab.com:41720/sneaker-head';
+const dbOptions = {
+  socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
+}
+
 console.log('connectString', connectionString);
 mongoose.connect(connectionString);
-mongoose.set('debug', true);
+var conn = mongoose.connection;
 
-seeder();
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function () {
+  seeder();
+  app.use(session({
+    secret: "top secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
+  }))
+
+  //passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(User.createStrategy());
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // routers
+  app.use('/users', usersRouter);
+  app.use('/auth', authRouter);
+  app.use('/products', productsRouter);
+  app.use('/cart', cartRouter);
+  app.use('/', indexRouter);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json(err)
+    // res.render('error');
+  });
+});
+
+
 
 //session
-app.use(session({
-  secret: "top secret",
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
-  })
-}))
 
-//passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// routers
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
-app.use('/products', productsRouter);
-app.use('/cart', cartRouter);
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json(err)
-  // res.render('error');
-});
 
 module.exports = app;
